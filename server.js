56fe4426068e8d04831616a2ffa503081acd30ef
Owen Rapaport <owenssrapaport@aol.com>
2015-04-18 01:40:45 -0400

    oauth works in very basic form

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var application_root = __dirname,
-    express = require("express"),
-    passport = require("passport"),
-    AngelListStrategy = require("passport-angellist").Strategy,
-    logger = require("morgan"),
+    express = require('express'),
+    passport = require('passport'),
+    util = require('util'),
     bodyParser = require("body-parser"),
-    path = require("path"),
+    cookieParser = require("cookie-parser"),
+    methodOverride = require("method-override"),
+    request = require("request"),
+    logger = require("morgan"),
     session = require("express-session"),
-    request = require("request");
-
-
-var app = express();
+    path = require("path"),
+    AngelListStrategy = require('passport-angellist').Strategy;
 
 var angelListClientID = "5b2e8928e19502edc3a05147d2a43c88ec57de4506c08b12";
 var angelListClientSecret = "38c5cdc20a10af63cffc39e15c65dcf99340107a02b8730f";
-var angelListAccessToken;
+var returnedAccessToken;
+
+var app = express();
 
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -26,34 +28,34 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new AngelListStrategy({
   clientID: angelListClientID,
   clientSecret: angelListClientSecret,
-  callbackURL: "http://localhost:3000/auth/angellist/callback"
+  callbackURL: "http://localhost:3000/logged_in"
 }, function(accessToken, refreshToken, profile, done) {
-  accessToken = angelListAccessToken;
-  console.log("Refresh Token:", refreshToken);
-  console.log("Profile", profile);
+  returnedAccessToken = accessToken;
   process.nextTick(function() {
     return done(null, profile);
   });
 }));
 
 app.use(logger("dev"));
+app.use(cookieParser());
 app.use(bodyParser());
+app.use(methodOverride());
 app.use(session({
   secret: "graeme",
   saveUninitialized: false,
   resave: false
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(application_root + '/public'));
+app.use(express.static(__dirname + '/public'));
+
+function ensureAuthenticated(req, res, next) { //Middleware for later
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
 
 app.get("/login", function(req, res) {
   res.send("<a href=\"/auth/angellist\">Log in with angeleijflse</a>");
-});
-
-app.get("/omg", function(req, res) {
-  res.send("OMG");
 });
 
 app.get("/toobad", function(req, res) {
@@ -65,18 +67,15 @@ app.get('/auth/angellist',
   function(req, res) {}
 );
 
-app.get("/logged_in", passport.authenticate("angellist", {failureRedirect: "/toobad" }),
+app.get('/logged_in', 
+  passport.authenticate('angellist', { failureRedirect: '/login' }),
   function(req, res) {
-
-    var aListPostURL = "https://angel.co/api/oauth/token?client_id=" + angelListClientID + 
-    "&client_secret=" + angelListClientSecret + "&code=" 
-    + req.query.code + "&grant_type=authorization_code";
-    
-    request.post(aListPostURL, function(error, response, body) {
-        console.log("Error:", error);
+    console.log("Code:", req.query.code);
+    console.log("Access:", returnedAccessToken);
+    request.get("https://api.angel.co/1/users/1159294/followers?access_token=" + returnedAccessToken,
+    function(error, response, body) {
         console.log("Body:", body);
-        //WTF?
-        res.send(response);
+      res.send(body);
     });
 });
 
