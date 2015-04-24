57f7f17f7ab8995c89546aeddc1b59d6c421b7a4
Owen Rapaport <owenssrapaport@aol.com>
2015-04-23 21:12:33 -0400

    herokuing

--- models/index.js
+++ models/index.js
@@ -6,7 +6,6 @@ var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 if (process.env.HEROKU_POSTGRESQL_GRAY_URL) {
   // the application is executed on Heroku ... use the postgres database
